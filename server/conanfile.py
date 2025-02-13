f1bb0df61dac0d59b2ae02c033cffc6a68b14121
Marek Macznik <marek.macznik@gmail.com>
2025-02-13 19:35:45 +0100

    Project skel

---
+++ server/conanfile.py
@@ -0,0 +1,36 @@
+from conan import ConanFile
+from conan.tools.cmake import (
+    CMake,
+    CMakeToolchain,
+    cmake_layout
+)
+
+class GridWarriors(ConanFile):
+    name = "gridwarriors"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+    options = {
+        "shared": [True, False]
+    }
+    default_options = {
+        "shared": False,
+    }
+    requires = [
+        "poco/1.13.3",
+        "gtest/1.15.0",
+    ]
+
+    def generate(self):
+        tc = CMakeToolchain(self, generator="Ninja")
+        tc.cache_variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = "ON"
+        tc.generate()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
